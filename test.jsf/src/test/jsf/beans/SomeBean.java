c3d999a829db5b7188cac44b52be7905d05044e0
u000501 <marc.schlegel@spb.de>
2015-03-23 16:20:04 +0100

    Pax-Web-Issue

---
+++ test.jsf/src/test/jsf/beans/SomeBean.java
@@ -0,0 +1,13 @@
+package test.jsf.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
+@ManagedBean
+public class SomeBean {
+
+	public String getHello(){
+		return "Hello World";
+	}
+}
